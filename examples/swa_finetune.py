fixing seeds for consistent reloading, back to masking with lieconv as it should be

examples/swa_finetune.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import DataLoader
-from oil.utils.utils import LoaderTo, cosLr, islice
+from oil.utils.utils import LoaderTo, cosLr, islice, FixedNumpySeed
 from oil.tuning.study import train_trial
 from oil.datasetup.datasets import split_dataset
 from oil.utils.parallel import try_multigpu_parallelize
@@ -23,12 +23,12 @@ def makeTrainer(*, dataset=MnistRotDataset, network=ImgLieResnet, num_epochs=100
                 trainer_config={'log_dir':None}):
 
     # Prep the datasets splits, model, and dataloaders
-    datasets = split_dataset(dataset(f'~/datasets/{dataset}/'),splits=split)
+    with FixedNumpySeed(0):
+        datasets = split_dataset(dataset(f'~/datasets/{dataset}/'),splits=split)
     datasets['test'] = dataset(f'~/datasets/{dataset}/', train=False)
     device = torch.device(device)
     model = network(num_targets=datasets['train'].num_targets,**net_config).to(device)
     if aug: model = torch.nn.Sequential(datasets['train'].default_aug_layers(),model)
-    model,bs = try_multigpu_parallelize(model,bs)
 
     dataloaders = {k:LoaderTo(DataLoader(v,batch_size=bs,shuffle=(k=='train'),
                 num_workers=0,pin_memory=False),device) for k,v in datasets.items()}

examples/train_molec.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
-from oil.utils.utils import LoaderTo, islice, cosLr
+from oil.utils.utils import LoaderTo, islice, cosLr, FixedNumpySeed
 from oil.tuning.args import argupdated_config
 from oil.tuning.study import train_trial
 from oil.utils.parallel import try_multigpu_parallelize
@@ -21,8 +21,9 @@ def makeTrainer(*, task='homo', device='cuda', lr=1e-2, bs=100, num_epochs=500,
                 subsample=False, trainer_config={'log_dir':None,'log_suffix':''}):#,'log_args':{'timeFrac':1/4,'minPeriod':0}}):
     # Create Training set and model
     device = torch.device(device)
-    datasets, num_species, charge_scale = QM9datasets()
-    if subsample: datasets.update(split_dataset(datasets['train'],{'train':subsample}))
+    with FixedNumpySeed(0):
+        datasets, num_species, charge_scale = QM9datasets()
+        if subsample: datasets.update(split_dataset(datasets['train'],{'train':subsample}))
     ds_stats = datasets['train'].stats[task]
     if recenter:
         m = datasets['train'].data['charges']>0