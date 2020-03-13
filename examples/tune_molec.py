c39849fe1cd811537655cbcbe91701331f4e5e95
mfinzi <mfinzi@hmc.edu>
2020-03-13 11:01:10 -0700

    tune_molec

--- examples/tune_molec.py
+++ examples/tune_molec.py
@@ -13,7 +13,7 @@ import lie_conv.lieGroups as lieGroups
 import lie_conv.lieConv as lieConv
 from lie_conv.lieConv import ImgLieResnet
 from lie_conv.datasets import MnistRotDataset
-from lie_conv.examples.train_molec import makeTrainer,Trial
+from examples.train_molec import makeTrainer,Trial
 from oil.tuning.study import Study
 
 def trial_name(cfg):
@@ -25,9 +25,10 @@ if __name__ == '__main__':
     config_spec.update({
         'num_epochs':500,
         'net_config':{'fill':[1/2,1.,3/4],'nbhd':[25,20,15],'liftsamples':4,
-                     'group':[lieGroups.SE3(a) for a in [.15,.2,.25]]},
-        'lr':[1e-3],'bs':75,'aug':True,'task':'homo',
-        'subsample':20000,
+                      'k':1536,'act':'swish','bn':True,'mean':True,'num_layers':6,
+                     'group':[lieGroups.SE3(a) for a in [.15,.2,.3]]},
+        'lr':[3e-3],'bs':75,'task':'homo',
+        'subsample':20000,'recenter':True,
         'save':True,
         'trainer_config':{'log_dir':'se3_molec_study','log_suffix':lambda cfg:trial_name(cfg)},
         
